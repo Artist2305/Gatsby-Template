8f5b08dc16f9adb192d6873e779ba408f4bce728
Mateusz Szostek <szostekm230590@gmail.com>
2021-03-17 14:02:01 +0000

    Purgecss integrated

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import './src/styles/global.css';
+import './src/styles/styles.css';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/state/store';
--- gatsby-config.js
+++ gatsby-config.js
@@ -128,5 +128,16 @@ module.exports = {
         accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        ignore: ['/customStyles.css', 'prismjs/', 'docsearch.js/', 'global.css'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    }
   ],
 };
